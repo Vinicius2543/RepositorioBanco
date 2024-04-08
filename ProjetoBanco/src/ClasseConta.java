0ab996286b50e431e9739b28b578a79446513a22
Vinícius Alexandre <107442699+Vinicius2543@users.noreply.github.com>
2024-04-08 20:02:31 -0300

    Segundo commit
    Adicionar os arquivos

---
+++ ProjetoBanco/src/ClasseConta.java
@@ -0,0 +1,10 @@
+
+public class ClasseConta {
+		short atributoNumeroConta;
+		float atributoSaldoConta;
+		String atributoTipoConta;
+		
+		String atributoNomeCliente;
+		int atributoDocumentoCliente;
+
+}
---
+++ ProjetoBanco/src/ClassePessoa.java
@@ -0,0 +1,24 @@
+
+public class ClassePessoa {
+
+	public static void main(String[] args) {
+		
+		ClasseConta objeto_conta_um; // cria uma variavel
+		objeto_conta_um = new ClasseConta(); // instancia o objeto
+		
+		ClasseConta objeto_conta_dois = new ClasseConta(); // cria a variavel e instancia o objeto(inline)
+		
+		System.out.println(objeto_conta_um.atributoNumeroConta);
+		System.out.println(objeto_conta_um.atributoSaldoConta);
+		System.out.println(objeto_conta_um.atributoTipoConta);
+		System.out.println(objeto_conta_um.atributoNomeCliente);
+		System.out.println(objeto_conta_um.atributoDocumentoCliente);
+		
+		System.out.println(objeto_conta_dois.atributoNumeroConta);
+		System.out.println(objeto_conta_dois.atributoSaldoConta);
+		System.out.println(objeto_conta_dois.atributoTipoConta);
+		System.out.println(objeto_conta_dois.atributoNomeCliente);
+		System.out.println(objeto_conta_dois.atributoDocumentoCliente);
+	}
+
+}
