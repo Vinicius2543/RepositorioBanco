843df9912d4c2db0d96343bf5dad3f7c7302a084
lab04 <lab04@lab4-20>
2024-04-08 20:25:14 -0300

    adicionando os dados

--- ProjetoBanco/src/ClassePrincipal.java
+++ ProjetoBanco/src/ClassePrincipal.java
@@ -2,8 +2,31 @@
 public class ClassePrincipal {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		ClasseConta objeto_conta_um; // cria uma variavel
+		objeto_conta_um = new ClasseConta(); // instancia o objeto
+		
+		ClasseConta objeto_conta_dois = new ClasseConta(); // cria a variavel e instancia o objeto(inline)
+		
+		objeto_conta_um.atributoNumeroConta = 123;
+		objeto_conta_um.atributoSaldoConta = 2_600.00F;
+		objeto_conta_um.atributoTipoConta = "Conta Poupança";
+		
+		objeto_conta_dois.atributoNumeroConta = 789;
+		objeto_conta_dois.atributoSaldoConta = 4_000.00F;
+		objeto_conta_dois.atributoTipoConta = "Conta Corrente";
+		
+		System.out.println("conta 1 numero da conta" + objeto_conta_um.atributoNumeroConta);
+		System.out.println(objeto_conta_um.atributoSaldoConta);
+		System.out.println(objeto_conta_um.atributoTipoConta);
+		System.out.println(objeto_conta_um.atributoNomeCliente);
+		System.out.println(objeto_conta_um.atributoDocumentoCliente);
+		
+		System.out.println(objeto_conta_dois.atributoNumeroConta);
+		System.out.println(objeto_conta_dois.atributoSaldoConta);
+		System.out.println(objeto_conta_dois.atributoTipoConta);
+		System.out.println(objeto_conta_dois.atributoNomeCliente);
+		System.out.println(objeto_conta_dois.atributoDocumentoCliente);
+		
 	}
 
 }
